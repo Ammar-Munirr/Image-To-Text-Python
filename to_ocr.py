0f2a257b5899a910510c02a6dc3a1cc77107d171
Able <munirammar0@gmail.com>
2023-06-17 01:17:59 +0500

    Adding some error handling to it

--- to_ocr.py
+++ to_ocr.py
@@ -1,19 +1,38 @@
 import os
-import pytesseract 
+import pytesseract
 from PIL import Image
-num = int(input("Total Images? "))
-os.system('clear')
-for x in range(1,num+1):
-    f = open('data.txt','a')
-    f.write('\n'+'###### Image '+str(x)+' Data #####'+'\n')
-    file = "image 1 "+"("+str(x)+")"+'.jpg'
-    image = Image.open(file)
-    text = pytesseract.image_to_string(image)
-    f = open('data.txt','a')
-    f.write(text+'\n')
-    f.write('######Image '+str(x)+' Data END #####'+'\n')
-    os.system('clear')
-    print('Progress: '+str(int((x/num)*100))+'%')
-f.close()
-print("Completed")
-a = input("Press Enter to Exit :)")
+
+
+def process_images(num_images, output_file):
+    try:
+        num_images = int(num_images)
+    except ValueError:
+        print("Invalid input. Please enter a valid number of images.")
+        return
+
+    output_file = output_file.strip()
+    if not output_file.endswith('.txt'):
+        output_file += '.txt'
+
+    with open(output_file, 'a') as f:
+        for x in range(1, num_images + 1):
+            image_file = f"image {x}.jpg"
+            if not os.path.exists(image_file):
+                print(f"Image '{image_file}' not found. Skipping...")
+                continue
+
+            f.write(f'\n###### Image {x} Data #####\n')
+            image = Image.open(image_file)
+            text = pytesseract.image_to_string(image)
+            f.write(text + '\n')
+            f.write(f'###### Image {x} Data END #####\n')
+            os.system('clear')
+            print(f'Progress: {int((x / num_images) * 100)}%')
+
+    print("Completed")
+
+
+if __name__ == '__main__':
+    num_images = input("Total Images? ")
+    process_images(num_images, 'data.txt')
+    input("Press Enter to Exit :)")
